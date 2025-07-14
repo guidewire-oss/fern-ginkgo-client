f3e04b8294fd2d45ac986e18a13b0340794a4866
Prakash V <mailprak@gmail.com>
2025-07-14 17:43:19 +0530

    fix: updating specs execution count

--- tests/ping_test.go
+++ tests/ping_test.go
@@ -2,44 +2,42 @@ package tests_test
 
 import (
 	"context"
-	"time"
 	"os"
+	"time"
 
+	pb "github.com/guidewire-oss/fern-ginkgo-client/reporter" // Import generated protobuf package
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"google.golang.org/grpc"
-	pb "github.com/guidewire-oss/fern-ginkgo-client/reporter" // Import generated protobuf package
 )
 
-var _ = Describe("PingService Real Test", func() {
+func init() {
+	if os.Getenv("GRPC_EXECUTE") == "TRUE" {
+		var _ = Describe("PingService Real Test", func() {
 
-	var (
-		client pb.PingServiceClient
-		conn   *grpc.ClientConn
-	)
+			var (
+				client pb.PingServiceClient
+				conn   *grpc.ClientConn
+			)
 
-	BeforeEach(func() {
+			BeforeEach(func() {
+				var err error
+				conn, err = grpc.Dial("127.0.0.1:50051", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
+				Expect(err).NotTo(HaveOccurred())
 
-		if os.Getenv("GRPC_EXECUTE") != "TRUE" {
-			Skip("Skipping real GRPC test as GRPC_EXECUTE != TRUE")
-		}
+				client = pb.NewPingServiceClient(conn)
+			})
 
-		var err error
-		conn, err = grpc.Dial("127.0.0.1:50051", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
-		Expect(err).NotTo(HaveOccurred())
+			AfterEach(func() {
+				conn.Close()
+			})
 
-		client = pb.NewPingServiceClient(conn)
-	})
-
-	AfterEach(func() {
-		conn.Close()
-	})
-
-	It("should send a real ping and get a response", func() {
-		resp, err := client.Ping(context.Background(), &pb.PingRequest{Message: "Pong"})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp).NotTo(BeNil())
-		Expect(resp.Message).To(Equal("Pong"))
-	})
-})
-
+			It("should send a real ping and get a response", func() {
+				resp, err := client.Ping(context.Background(), &pb.PingRequest{Message: "Pong"})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(resp).NotTo(BeNil())
+				Expect(resp.Message).To(Equal("Pong"))
+			})
+		})
+	}
+}
