be948b9348a33f703fea0c1c89105043e593a0a4
cwrm56 <43615527+cwrm56@users.noreply.github.com>
2025-07-25 13:48:40 -0700

    fix: send spec tags at spec level and suite at suite level #10
    Signed-off-by: sundarok <38673525+sundarok@users.noreply.github.com>

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -10,7 +10,7 @@ type TimeLog struct {
 }
 
 type TestRun struct {
-	ID                uint64     `json:"id" gorm:"primaryKey"`
+	ID                uint64     `json:"id"`
 	TestProjectName   string     `json:"test_project_name"`
 	TestProjectID     string     `json:"test_project_id"`
 	TestSeed          uint64     `json:"test_seed"`
@@ -20,30 +20,31 @@ type TestRun struct {
 	GitSha            string     `json:"git_sha"`
 	BuildTriggerActor string     `json:"build_trigger_actor"`
 	BuildUrl          string     `json:"build_url"`
-	SuiteRuns         []SuiteRun `json:"suite_runs" gorm:"foreignKey:TestRunID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	SuiteRuns         []SuiteRun `json:"suite_runs"`
 }
 
 type SuiteRun struct {
-	ID        uint64    `json:"id" gorm:"primaryKey"`
+	ID        uint64    `json:"id"`
 	TestRunID uint64    `json:"test_run_id"`
 	SuiteName string    `json:"suite_name"`
 	StartTime time.Time `json:"start_time"`
 	EndTime   time.Time `json:"end_time"`
-	SpecRuns  []SpecRun `json:"spec_runs" gorm:"foreignKey:SuiteID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Tags      []Tag     `json:"tags"`
+	SpecRuns  []SpecRun `json:"spec_runs"`
 }
 
 type SpecRun struct {
-	ID              uint64    `json:"id" gorm:"primaryKey"`
+	ID              uint64    `json:"id"`
 	SuiteID         uint64    `json:"suite_id"`
 	SpecDescription string    `json:"spec_description"`
 	Status          string    `json:"status"`
 	Message         string    `json:"message"`
-	Tags            []Tag     `json:"tags" gorm:"many2many:spec_run_tags;"`
 	StartTime       time.Time `json:"start_time"`
 	EndTime         time.Time `json:"end_time"`
+	Tags            []Tag     `json:"tags"`
 }
 
 type Tag struct {
-	ID   uint64 `json:"id" gorm:"primaryKey"`
+	ID   uint64 `json:"id"`
 	Name string `json:"name"`
 }
